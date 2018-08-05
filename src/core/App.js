20b7739dc292be555e92674bf440a1f4355d0439
thanawat <tha_chookes@hotmail.com>
2018-08-05 16:13:57 +0700

    support decorator

--- src/core/App.js
+++ src/core/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Provider } from 'mobx-react'
+
 import Routes from './routes'
+import Stores from './stores' 
 class App extends Component {
   render() {
     return (
       <div>
-        <Routes />
+        <Provider {...Stores} >
+          <Routes />
+        </Provider>
       </div>
     )
   }
---
+++ src/core/stores/index.js
@@ -0,0 +1,9 @@
+import ExampleStore from './ExampleStore'
+
+const stores = {
+  ExampleStore: new ExampleStore(),
+  // ConfigStore: new ConfigStore(),
+  // locale: new LocaleStore('en')
+}
+
+export default stores
