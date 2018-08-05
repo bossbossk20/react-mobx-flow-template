1d16ee72bd06eda27654941f41f56b9678eb62c8
thanawat <tha_chookes@hotmail.com>
2018-08-05 14:53:09 +0700

    add hot loader

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    ReactDOM.render(<App />, document.getElementById('root'))
+  })
+}
