4958ab828146c1227446fd6414395c6e2e613d4b
thanawat <tha_chookes@hotmail.com>
2018-08-05 22:06:32 +0700

    add mobx example

--- src/pages/page1/index.js
+++ src/pages/page1/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Button from '../../components/Button'
+import { compose } from 'recompose'
+import { inject, observer } from 'mobx-react'
 
 type Props = {}
 
-const Page1 = (props: Props) => (
-  <div>
-    <h1>Page1 </h1>
-    <Button text='Click' />>
-  </div>
-)
+const Page1 = (props: Props) => {
+  const { ExampleStore } = props
+  return (
+    <div>
+      <h1>{ExampleStore.exampleData.text}</h1>
+      <h1>Page1 </h1>
+      <Button text='Click' />
+    </div>
+  )
+}
 
-export default Page1
+
+export default compose(
+  inject('ExampleStore'),
+  observer
+)(Page1)
