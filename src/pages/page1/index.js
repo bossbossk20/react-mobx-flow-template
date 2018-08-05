41814387b1a5f3f26ce1d24041e6c6db9a97e071
thanawat <tha_chookes@hotmail.com>
2018-08-05 14:48:34 +0700

    init react router & prettier format code

--- src/pages/page1/index.js
+++ src/pages/page1/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import Routes from './routes'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Routes />
+      <div >
+        <h1>
+          Page1
+        </h1>
       </div>
     )
   }
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, } from 'react-router-dom'
+
+import Page1 from './pages/page1'
+import Page2 from './pages/page2'
+
+const Routes = () => (
+  <Router>
+    <div>
+      <Route path='/page1' component={Page1} />
+      <Route path='/page2' component={Page2} />
+    </div>
+  </Router>
+)
+
+export default Routes
